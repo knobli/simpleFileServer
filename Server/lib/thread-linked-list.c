3f689e67e4e43c98cd207b265d76a23759c121f9
knobli <raffael@santschi.ch>
2014-06-13 17:17:38 +0200

    first draft of dequeue

--- Server/lib/thread-linked-list.c
+++ Server/lib/thread-linked-list.c
@@ -19,10 +19,12 @@ struct thread_element {
 	int thread_idx;
 	pthread_mutex_t link_mod_mutex;
 	pthread_t thread;
-	struct thread_element *next;
+	struct thread_element *prev;
 };
 
+pthread_mutex_t head_thread_mutex;
 struct thread_element *head_thread = NULL;
+struct thread_element *end_thread = NULL;
 
 struct thread_element* create_thread_element(int thread_idx, pthread_t thread) {
 	const int deep = 4;
@@ -35,7 +37,7 @@ struct thread_element* create_thread_element(int thread_idx, pthread_t thread) {
 	}
 	thread_element->thread_idx = thread_idx;
 	thread_element->thread = thread;
-	thread_element->next = NULL;
+	thread_element->prev = NULL;
 
 	int returnCode;
 	debug(deep, "Init link mod mutex");
@@ -53,93 +55,46 @@ void init_thread_linked_list() {
 	int deep = 1;
 	debug(deep, "Init thread linked list");
 	head_thread = create_thread_element(0, 0);
-}
-
-struct thread_element* search_thread_element(int thread_idx, struct thread_element **prev,
-		int lock) {
-	const int deep = 4;
-	int returnCode;
-	struct thread_element *ptr = head_thread;
-	struct thread_element *last = NULL;
-	struct thread_element *second_last = NULL;
-	bool found = false;
-	info(deep, "Searching thread element '%d' in the list", thread_idx);
-	while (ptr != NULL) {
-		if (second_last != NULL && lock) {
-			debug(deep, "Unlock second last link mod mutex on %p - search case",
-					second_last);
-			//debug(deep, "Link mod mutex %p - search case", &second_last->link_mod_mutex);
-			returnCode = pthread_mutex_unlock(&second_last->link_mod_mutex);
-			handle_thread_error(returnCode,
-					"Could not release link mod mutex - search", THREAD_EXIT);
-		}
-		debug(deep, "Lock current link mod mutex on %p - search case", ptr);
-		//debug(deep, "Link mod mutex %p - search case", &ptr->link_mod_mutex);
-		returnCode = pthread_mutex_lock(&ptr->link_mod_mutex);
-		handle_thread_error(returnCode,
-				"Could not lock link mod mutex - search", THREAD_EXIT);
-		if (ptr->thread_idx == thread_idx) {
-			debug(deep, "Thread element '%d' found!", thread_idx);
-			found = true;
-
-			if(lock == true){
-				debug(deep, "Unlock matching link mod mutex on %p - search case", ptr);
-				//debug(deep, "Link mod mutex %p - search ptr", &ptr->link_mod_mutex);
-				returnCode = pthread_mutex_unlock(&ptr->link_mod_mutex);
-				handle_thread_error(returnCode,
-						"Could not release link mod mutex - search", THREAD_EXIT);
-			}
-
-			break;
-		} else {
-			debug(deep, "Set second_last to: %p", last);
-			second_last = last;
-			debug(deep, "Set last to: %p", ptr);
-			last = ptr;
-			debug(deep, "Set ptr to: %p", ptr->next);
-			ptr = ptr->next;
-		}
-	}
-
-	if (second_last != NULL && lock) {
-		debug(deep, "Unlock second last link mod mutex on %p - search case",
-				second_last);
-		//debug(deep, "Link mod mutex %p - search case", &second_last->link_mod_mutex);
-		returnCode = pthread_mutex_unlock(&second_last->link_mod_mutex);
-		handle_thread_error(returnCode,
-				"Could not release link mod mutex - search", THREAD_EXIT);
-	}
-
-	if (prev) {
-		debug(deep, "Set previous: %p", last);
-		*prev = last;
-	}
-
-	if (true == found) {
-		return ptr;
-	} else {
-		return NULL;
-	}
+	end_thread = head_thread;
 }
 
 int add_thread_element(int thread_idx, pthread_t thread) {
 	const int deep = 3;
 	int rv;
-	struct thread_element *ptr = create_thread_element(thread_idx, thread);
-	struct thread_element *endNode = NULL;
-	struct thread_element *thread_element = search_thread_element(thread_idx, &endNode, true);
-	if (thread_element == NULL) {
-		info(deep, "Adding thread element '%d' to the end of the list", thread_idx);
-		debug(deep, "Set next pointer of %p to new thread element %p", endNode, ptr);
-		endNode->next = ptr;
-		rv = true;
-	} else {
-		error(deep, "Thread element '%d' already exist", thread_idx);
-		rv = false;
-	}
+	struct thread_element *thread_element = create_thread_element(thread_idx, thread);
+	int returnCode;
 
-	debug(deep, "Unlock list mod mutex on %p - create case", endNode);
-	int returnCode = pthread_mutex_unlock(&endNode->link_mod_mutex);
+	debug(deep, "Lock head thread mutex - add case");
+	returnCode = pthread_mutex_lock(&head_thread_mutex);
+	handle_thread_error(returnCode,
+			"Could not lock link mod mutex - add", THREAD_EXIT);
+
+	debug(deep, "Lock head thread link mod mutex - add case", head_thread);
+	returnCode = pthread_mutex_lock(&head_thread->link_mod_mutex);
+	handle_thread_error(returnCode,
+			"Could not lock link mod mutex - search", THREAD_EXIT);
+	/*
+	 * Debug
+	 */
+	if(head_thread->prev != NULL){
+		error(deep, "Previous of head thread not NULL!");
+	}
+	/*
+		 * Debug
+		 */
+
+	info(deep, "Adding thread element '%d' to the beginning of the list", thread_idx);
+	head_thread->prev = thread_element;
+	head_thread = thread_element;
+	rv = true;
+
+	debug(deep, "Unlock head thread link mod mutex - add casee", head_thread);
+	int returnCode = pthread_mutex_unlock(&head_thread->link_mod_mutex);
+	handle_thread_error(returnCode, "Could not release list mod mutex - create",
+			THREAD_EXIT);
+
+	debug(deep, "Unlock head thread mutex - add case");
+	int returnCode = pthread_mutex_unlock(&head_thread_mutex);
 	handle_thread_error(returnCode, "Could not release list mod mutex - create",
 			THREAD_EXIT);
 	return rv;
@@ -147,48 +102,44 @@ int add_thread_element(int thread_idx, pthread_t thread) {
 
 int clean_up_threads() {
 	const int deep = 3;
-	struct thread_element *ptr = head_thread;
-	struct thread_element *last = NULL;
+	struct thread_element *ptr = end_thread->prev;
+	struct thread_element *last = end_thread;
 	int returnCode;
 	info(deep, "Go trough thread elements");
 	while (ptr != NULL) {
-		if (last != NULL) {
-			debug(deep, "Release last link mod mutex on %p - list case", last);
-			returnCode = pthread_mutex_unlock(&last->link_mod_mutex);
-			handle_thread_error(returnCode, "Could not release link mod mutex",
-					THREAD_EXIT);
-		}
-		debug(deep, "Lock link mod mutex on %p - list case", ptr);
-		returnCode = pthread_mutex_lock(&ptr->link_mod_mutex);
-		handle_thread_error(returnCode, "Could not lock link mod mutex",
+		debug(deep, "Join thread '%d'", ptr->thread_idx);
+		pthread_join(ptr->thread, NULL);
+
+		debug(deep, "Lock link mod mutex on %p - cleanup case", last);
+		returnCode = pthread_mutex_lock(&last->link_mod_mutex);
+		handle_thread_error(returnCode, "Could not lock link mod mutex - cleanup",
 				THREAD_EXIT);
 
-		if (ptr->thread_idx != 0) {
-			debug(deep, "Join thread '%d'", ptr->thread_idx);
-			pthread_join(ptr->thread, NULL);
-			debug(deep, "Delete thread '%d'", ptr->thread_idx);
-			last->next = ptr->next;
-			debug(deep, "Unlock link mod mutex on %p - delete case", last);
-			returnCode = pthread_mutex_unlock(&last->link_mod_mutex);
-			handle_thread_error(returnCode, "Could not release link mod mutex - delete",
+		if(ptr->prev == NULL){
+			debug(deep, "Special case: remove last element from list");
+			debug(deep, "Lock head thread link mod mutex - cleanup case");
+			returnCode = pthread_mutex_lock(&head_thread_mutex);
+			handle_thread_error(returnCode, "Could not lock link mod mutex - cleanup",
 					THREAD_EXIT);
-			debug(deep, "Free pointer %p of thread element", ptr);
-			free(ptr);
-			ptr = NULL;
-			debug(deep, "Set pointer %p as curr pointer", last->next);
-			ptr = last->next;
-		} else {
-			last = ptr;
-			ptr = ptr->next;
 		}
 
-	}
-	if (last != NULL) {
-		debug(deep, "Release last link mod mutex on %p - list case", last);
+		last->prev = ptr->prev;
+
+		if(ptr->prev == NULL){
+			debug(deep, "Unlock head thread link mod mutex - cleanup case");
+			returnCode = pthread_mutex_unlock(&head_thread_mutex);
+			handle_thread_error(returnCode, "Could not lock link mod mutex - cleanup",
+					THREAD_EXIT);
+		}
+
+		debug(deep, "Unlock last link mod mutex on %p - list case", last);
 		returnCode = pthread_mutex_unlock(&last->link_mod_mutex);
-		handle_thread_error(returnCode, "Could not release link mod mutex",
+		handle_thread_error(returnCode, "Could not release link mod mutex - cleanup",
 				THREAD_EXIT);
-	}
 
+		free(ptr);
+
+		ptr = ptr->prev;
+	}
 	return true;
 }
